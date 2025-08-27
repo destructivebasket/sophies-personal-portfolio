305713624545fc1893d3c602f35598e745ceecd6
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-08-27 16:04:36 -0700

    added some links

--- sophies-personal-portfolio/src/Components/HomePage/HomePage.jsx
+++ sophies-personal-portfolio/src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,9 @@
 import './HomePage.css'
 import NavBar from '../NavBar/Navbar';
 import { Link } from 'react-router-dom';
+import LinkedIn from '../../assets/linkedin.png'
+import GitHub from '../../assets/github.png'
+import Email from '../../assets/email.jpg'
 
 const HomePage = () => {
 
@@ -9,6 +12,7 @@ const HomePage = () => {
       
       <NavBar/>
 
+      {/* Intro Text */}
       <div className = "flex flex-col absolute left-0 top-50 ml-20">
         <p id = "hello"> Hello! </p>
         <h1 id = "name"> I'm <span className = "gradient"> Sophie Choi </span> </h1>
@@ -20,13 +24,31 @@ const HomePage = () => {
         >
           <button id="resume-button">Resume</button>
         </a>
-        <a 
-          href="https://drive.google.com/file/d/1TtDaoL9nrAMLa2ce_MWDm7kJxx088VgI/view?usp=sharing" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          <img src = ""/> 
-        </a>
+
+        {/* Socials Icons */}
+        <div className = "flex items-center">
+          <a 
+            href="www.linkedin.com/in/sophie-choi-b78991319" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <img src = {LinkedIn}/> 
+          </a>
+          <a 
+            href="https://github.com/destructivebasket" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <img src = {GitHub}/> 
+          </a>
+          <a 
+            href="mailto:sophie.elissa.choi@gmail.com" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <img src = {Email}/> 
+          </a>
+        </div>
       </div>
     </div>
   )
