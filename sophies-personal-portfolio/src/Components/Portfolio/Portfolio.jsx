49e5a45c014d0c79899b9509068e3cccd4b08b0b
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-09-07 00:55:22 -0700

    finished portfolio page

--- sophies-personal-portfolio/src/Components/Portfolio/Portfolio.jsx
+++ sophies-personal-portfolio/src/Components/Portfolio/Portfolio.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
 import NavBar from '../NavBar/Navbar';
+import './Portfolio.css';
+import PortfolioCard from './PortfolioCard';
+import Emageo from '../../assets/emageo.png';
 
 const Portfolio = () => {
   return (
     <div>
       <NavBar/>
-      Portfolio
+        <div className = "translate-y-10">
+          <h1 className = "skills gradient max-md:translate-x-10 mb-10 max-sm:-translate-x-7"> Portfolio </h1>
+          <div className = "space-y-5">
+            <PortfolioCard
+              title = "Emageo"
+              description = {`Designed and developed a web travel planner using React, Ionic, and Tailwind CSS frameworks\n
+                            Implemented AviationStack and SerpAPI to fetch real-time travel data and Google Maps locations\n
+                            Integrated Firebase and Firestore for secure and scalable user data management\n
+                            Deployed website on Cloudflare and backend server on Render`}
+              picture = {Emageo}
+              link = "https://emageo.pages.dev/"
+            />
+
+            <PortfolioCard
+              title = "Mathclopedia"
+              description = {`Developed a math-assist platform using HTML, CSS, and JavaScript, hosted on Github Pages
+                              Designed an intuitive interface with categorized tabs, ranging from Algebra to Calculus concepts`}
+              link = "https://mathclopedia.github.io/"
+            />
+
+            <PortfolioCard
+              title = "Jellyfish Clock"
+              description = {`Implemented a real-time clock local to the user’s timezone, deployed on iOS with Capacitor, Xcode and Github Pages`}
+              link = "https://destructivebasket.github.io/jellyfish-clock/"
+            />
+
+            <PortfolioCard
+              title = "Movie App"
+              description = {`Implemented a real-time clock local to the user’s timezone, deployed on iOS with Capacitor, Xcode and Github Pages`}
+              link = "https://destructivebasket.github.io/movie-app-new/"
+            />
+
+            <div className = "mb-10">
+              <PortfolioCard
+                title = "Station 8"
+                description = {`Developed a horror game with Unity and C#, implementing AI pathfinding via NavMesh library
+                                Designed gameplay and storyboarding using Unity Timeline for cutscenes and sprite animations`}
+                link = "https://www.youtube.com/watch?v=ql5_dtAqs_A&feature=youtu.be"
+              />
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
